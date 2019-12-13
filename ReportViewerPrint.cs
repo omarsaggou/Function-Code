1363fc1715087e08fd4649d2f7dbfa27362ce598
OMAR SAGGOU <omarsaggou@gmail.com>
2019-12-13 16:21:51 +0100

    reduce

--- ReportViewerPrint.cs
+++ ReportViewerPrint.cs
@@ -12,29 +12,29 @@ using System.Windows;
     public class ReportViewer
     {
         public ReportViewer() { }
-        public void PrintPurchaseOrder(ReportClass rep, object o){
-            /// checking kind of repport before printing
-            if (rep is ReportName){
-                printPurchaseOrder((ClassName)o);
-            }            
-        }
-
+        
         ///method to print reoprt after adding parameters
-        public void PrintRepot(ClassName cln){
+        public void PrintReport(Object obj){
             try{
-                var r = new SalesOrderReport();
-                ParameterFieldDefinitions pfds;
-                ParameterValues pvs;
-                ////
-                Parameterprint(r, cln.AttributeName, "AttributeName",pfds,pvs);                
-                r.ReadRecords();
-                r.PrintToPrinter(2, true, 0, 0);
-            }
-            catch (Exception exp){
+                if (obj is ClassName)
+                {
+                    var o = (ClassName)obj;
+                    var r = new ClassNameReport();
+                    ParameterFieldDefinitions pfds = null;
+                    ParameterValues pvs = null;
+                    ////
+                    Parameterprint(r, o.ClassNameNumber, "ClassNameNumber", pfds, pvs);
+                    Parameterprint(r, o.ClassNameId, "ClassNameId", pfds, pvs);
+                    string dt1 = o.poDate.ToString("yyyy/MM/dd");
+                    Parameterprint(r, dt1, "poDate", pfds, pvs);
+                    r.ReadRecords();
+                    r.PrintToPrinter(2, true, 0, 0);
+                }
+            catch(Exception exp){
                 MessageBox.Show("l'erreur est " + exp.Message + " " + exp.InnerException);
+                }
             }
         }
-
         //// method to add parameter manipuled on report
         public void Parameterprint(ReportClass f, Object o, string parameternameParameterFieldDefinitions pfds,ParameterValues pvs){
             try{
