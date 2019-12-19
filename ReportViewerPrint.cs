5475b385c24c622f8ddf7012b47b5cd5925b2537
OMAR SAGGOU <omarsaggou@gmail.com>
2019-12-19 12:39:54 +0100

    add redifinition print method

--- ReportViewerPrint.cs
+++ ReportViewerPrint.cs
@@ -14,6 +14,7 @@ using System.Windows;
         public ReportViewer() { }
         
         ///method to print reoprt after adding parameters
+        /// Print with one object
         public void PrintReport(Object obj){
             try{
                 if (obj is ClassName)
@@ -35,6 +36,38 @@ using System.Windows;
                 }
             }
         }
+
+
+
+        public void PrintReport(Object obj1,Object obj2,Object obj3,Object obj4){
+            try{
+                if ( (obj1 is CLass1) && ( obj2is is Class2)&&(obj3 is Class3)&&(obj4 is Class4)){
+                    var obj_1 = (CLass1)obj1;
+                    var obj_2=(Class2)obj2;
+                    var obj_3=(Class3)obj3;
+                    var obj_4=(Class4)obj4;
+                    var r = new PurchaseOrderReport();
+                    ParameterFieldDefinitions pfds = null;
+                    ParameterValues pvs = null;
+                    //// CLass1
+                    Parameterprint(r, obj_1.Attribute, "Attribute", pfds, pvs);
+                    string dt1 = obj_1.date.ToString("yyyy/MM/dd");
+                    Parameterprint(r, dt1, "date", pfds, pvs);
+                    ///Class3
+                    Parameterprint(r,obj_3.billingContactPerson,"billingContactPerson",pfds,pvs);
+                    ///Class2
+                    Parameterprint(r,obj_2.Class2Name,"Class2Name",pds,pvs);
+                    ///Class4
+                    Parameterprint(r,obj_4.CurrencyName,"CurrencyName"pfds,vs);
+                    r.PrintToPrinter(2, true, 0, 0);
+                }
+            catch(Exception exp){
+                MessageBox.Show("l'erreur est " + exp.Message + " " + exp.InnerException);
+                }
+            }
+        }
+
+
         //// method to add parameter manipuled on report
         public void Parameterprint(ReportClass f, Object o, string parameternameParameterFieldDefinitions pfds,ParameterValues pvs){
             try{
